6b73198bfa51f85a7adea8f18b58610b65c96ee5
ponday-dev <ponday.dev+github@gmail.com>
2018-01-09 00:07:19 +0900

    [add] function: authorize

--- src/Qiita/Endpoints.elm
+++ src/Qiita/Endpoints.elm
@@ -5,6 +5,16 @@ baseUrl =
     "https://qiita.com"
 
 
+authorize: String -> String -> String
+authorize clientId scope =
+    baseUrl
+        ++ "/api/v2/oauth/authorize"
+        ++ "?client_id="
+        ++ clientId
+        ++ "&scope="
+        ++ scope
+
+
 accessTokens: String
 accessTokens =
     baseUrl ++ "/api/v2/access_tokens"
