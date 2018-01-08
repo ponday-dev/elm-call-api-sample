ca8efcf542f2e559898ceb1bb34ff2651f6e7a91
ponday-dev <ponday.dev+github@gmail.com>
2018-01-08 18:07:28 +0900

    [add] Qiita module

---
+++ src/Qiita/AccessToken.elm
@@ -0,0 +1,62 @@
+module Qiita.AccessToken exposing
+    ( Request
+    , Response
+    , getAccessToken
+    )
+
+import Http
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (requiredAt)
+import Json.Encode as Encode
+
+import Qiita.Endpoints exposing (accessTokens)
+
+
+-- request of POST /api/v2/access_tokens
+type alias Request =
+    { clientId: String
+    , clientSecret: String
+    , code: String
+    }
+
+
+-- response of POST /api/v2/access_tokens
+type alias Response =
+    { clientId: String
+    , scopes: List String
+    , token: String
+    }
+
+
+-- encoder
+requestEncoder: Request -> Encode.Value
+requestEncoder request =
+    Encode.object
+        [ ("client_id", Encode.string request.clientId)
+        , ("client_secret", Encode.string request.clientSecret)
+        , ("code", Encode.string request.code)
+        ]
+
+
+-- decoder
+responseDecoder: Decoder Response
+responseDecoder =
+    Decode.succeed Response
+        |> requiredAt [ "client_id" ] Decode.string
+        |> requiredAt [ "scopes" ] (Decode.list Decode.string)
+        |> requiredAt [ "token" ] Decode.string
+
+
+-- post request
+getAccessToken: (Result Http.Error Response -> a) -> Request -> Cmd a
+getAccessToken msg requestBody =
+    let
+        encoder =
+            requestEncoder requestBody
+        body =
+            Http.stringBody "application/json" (Encode.encode 4 encoder)
+        request =
+            Http.post accessTokens body responseDecoder
+    in
+        Http.send msg request
+        
---
+++ src/Qiita/Endpoints.elm
@@ -0,0 +1,10 @@
+module Qiita.Endpoints exposing (..)
+
+baseUrl: String
+baseUrl =
+    "https://qiita.com"
+
+
+accessTokens: String
+accessTokens =
+    baseUrl ++ "/api/v2/access_tokens"
